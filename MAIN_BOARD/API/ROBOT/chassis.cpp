6400089fbe3a3bc2324bb30abf37f0fed0235098
Hongjin Chen <jin013ch@gmail.com>
2023-08-02 17:05:31 +0800

    修改bug

--- MAIN_BOARD/API/ROBOT/chassis.cpp
+++ MAIN_BOARD/API/ROBOT/chassis.cpp
@@ -2,39 +2,38 @@
 
 C_Omniwheel_Motors Omni_chassis[4];
 
-float C_Omniwheel_Motors::cal_single_feed_forward(C_VECTOR &expect_Velt, const int num)
-{
+float C_Omniwheel_Motors::cal_single_feed_forward(C_VECTOR &expect_Velt, const int num) {
     static C_VECTOR d_straight_velt, pre_straight_velt, straight_velt;
     float d_straight_velt_dis;
     float feed_forward_current;
     straight_velt = expect_Velt;
     // 得到加速度d_straight_velt，单位cm/s2
     d_straight_velt = C_VECTOR((straight_velt.fpLength - pre_straight_velt.fpLength) / 0.002f,
+                               straight_velt.fpthetha, POLAR);
     // 将加速度(x，y方向的分量)沿四个轮的方向进行分解
-    switch (num)
-    {
-    case RIGHTUP:
-        d_straight_velt_dis =
-            d_straight_velt.fpVy / 2.0f / L_HALF * R_HALF - d_straight_velt.fpVx / 2.0f / B_HALF * R_HALF;
-        feed_forward_current = d_straight_velt_dis * K_RU_STRAIGHT + START_CURRENT_RU;
-        break;
-    case LEFTUP:
-        d_straight_velt_dis = -d_straight_velt.fpVy / 2.0f / L_HALF * R_HALF -
-                              d_straight_velt.fpVx / 2.0f / B_HALF * R_HALF;
-        feed_forward_current = d_straight_velt_dis * K_LU_STRAIGHT + START_CURRENT_LU;
-        break;
-    case LEFTDOWN:
-        d_straight_velt_dis = -d_straight_velt.fpVy / 2.0f / L_HALF * R_HALF +
-                              d_straight_velt.fpVx / 2.0f / B_HALF * R_HALF;
-        feed_forward_current = d_straight_velt_dis * K_LD_STRAIGHT + START_CURRENT_LD;
-        break;
-    case RIGHTDOWN:
-        d_straight_velt_dis =
-            d_straight_velt.fpVy / 2.0f / L_HALF * R_HALF + d_straight_velt.fpVx / 2.0f / B_HALF * R_HALF;
-        feed_forward_current = d_straight_velt_dis * K_RD_STRAIGHT + START_CURRENT_RD;
-        break;
-    default:
-        break;
+    switch (num) {
+        case RIGHTUP:
+            d_straight_velt_dis =
+                d_straight_velt.fpVy / 2.0f / L_HALF * R_HALF - d_straight_velt.fpVx / 2.0f / B_HALF * R_HALF;
+            feed_forward_current = d_straight_velt_dis * K_RU_STRAIGHT + START_CURRENT_RU;
+            break;
+        case LEFTUP:
+            d_straight_velt_dis = -d_straight_velt.fpVy / 2.0f / L_HALF * R_HALF -
+                                  d_straight_velt.fpVx / 2.0f / B_HALF * R_HALF;
+            feed_forward_current = d_straight_velt_dis * K_LU_STRAIGHT + START_CURRENT_LU;
+            break;
+        case LEFTDOWN:
+            d_straight_velt_dis = -d_straight_velt.fpVy / 2.0f / L_HALF * R_HALF +
+                                  d_straight_velt.fpVx / 2.0f / B_HALF * R_HALF;
+            feed_forward_current = d_straight_velt_dis * K_LD_STRAIGHT + START_CURRENT_LD;
+            break;
+        case RIGHTDOWN:
+            d_straight_velt_dis =
+                d_straight_velt.fpVy / 2.0f / L_HALF * R_HALF + d_straight_velt.fpVx / 2.0f / B_HALF * R_HALF;
+            feed_forward_current = d_straight_velt_dis * K_RD_STRAIGHT + START_CURRENT_RD;
+            break;
+        default:
+            break;
     }
     pre_straight_velt = straight_velt;
     return feed_forward_current;
--- MAIN_BOARD/API/ROBOT/chassis.h
+++ MAIN_BOARD/API/ROBOT/chassis.h
@@ -5,20 +5,21 @@
 #include "pid_algorithm.h"
 
 // 8矢量底盘需要的尺寸，单位mm
-#define L_HALF 308.63f / 1.414f // 半车长，指X方向
-#define B_HALF 308.63f / 1.414f // 半车宽，指Y方向
+#define L_HALF 308.63f / 1.414f  // 半车长，指X方向
+#define B_HALF 308.63f / 1.414f  // 半车宽，指Y方向
 #define R_HALF 308.63f
+#define R_WHEEL 76.0f  // 44.0f//适当给小R_WHEEL，这样可以使except_global_velt小于velt_fb，达到想要的效果
 
-#define OMNI_GEAR_RATIO 19.0f // 全向轮减速比
+#define OMNI_GEAR_RATIO 19.0f  // 全向轮减速比
 
 // 全向轮参数
-#define OMNI_VELT_KP 1500 // 450
+#define OMNI_VELT_KP 1500  // 450
 #define OMNI_VELT_KI 5    // 0
 #define OMNI_VELT_KD 0
 #define OMNI_VELT_UMax 20000.0f
 #define OMNI_VELT_Ts 0.001f
 
-/*feedforward*/ // 24.5072
+/*feedforward*/  // 24.5072
 
 #define K_RU_STRAIGHT 0.0f
 #define K_LU_STRAIGHT 0.0f
@@ -40,24 +41,12 @@
 
 #define START_CURRENT_UP 2.180650738014755e+03
 
-enum tri_chassis_e
-{
-    T_UP,
-    T_RIGHTDOWN,
-    T_LEFTDOWN
-};
+enum tri_chassis_e { T_UP, T_RIGHTDOWN, T_LEFTDOWN };
 
-enum chassis_e
-{
-    RIGHTUP,
-    LEFTUP,
-    LEFTDOWN,
-    RIGHTDOWN
-};
+enum chassis_e { RIGHTUP, LEFTUP, LEFTDOWN, RIGHTDOWN };
 
-class C_Omniwheel_Motors
-{
-public:
+class C_Omniwheel_Motors {
+   public:
     C_Motor m_run_motor;
 
     C_Omniwheel_Motors()
